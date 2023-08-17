4581d9cd19a9833386b2e47a76f55a2db9503631
staimov <staimov@mail.ru>
2023-08-17 22:35:13 +0500

    '//language=SQL' added

--- src/main/java/com/game/config/DBConstants.java
+++ src/main/java/com/game/config/DBConstants.java
@@ -7,5 +7,7 @@ public class DBConstants {
     public static final String DB_HBM2DDL_AUTO = "update";
     public static final String DB_DRIVER = "com.p6spy.engine.spy.P6SpyDriver";
     public static final String DB_URL = "jdbc:p6spy:mysql://localhost:3306/rpg";
+
+    //language=SQL
     public static final String SELECT_ALL_PLAYERS_SQL = "select * from rpg.player order by id";
 }
